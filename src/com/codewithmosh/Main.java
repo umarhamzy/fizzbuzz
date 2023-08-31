e0f2da4f403c99a83e38995188fbc9c4b43dddd6
umarhamzy <138566943+umarhamzy@users.noreply.github.com>
2023-08-31 11:46:10 +0530

    Updates Fizzbuzz program to include complex looping

--- src/com/codewithmosh/Main.java
+++ src/com/codewithmosh/Main.java
@@ -9,20 +9,28 @@ public class Main {
     String fizz = "Fizz";
     String buzz = "Buzz";
     
-    System.out.print("Number: ");
-    int number = scanner.nextInt();
+    System.out.print("How many times do you want to run the program? \nType a value less than or equal to 7: ");
+    int numberOfInput = scanner.nextInt();
     
-    if (number % 5 == 0 && number % 3 == 0) {
-      System.out.println(fizz + buzz);
+    while (numberOfInput > 0 && numberOfInput <= 7) {
+      for (int i = 0; i <= numberOfInput && numberOfInput > 0; i++) {
+        System.out.print("Number: ");
+        int number = scanner.nextInt();
+        
+        if (number % 5 == 0 && number % 3 == 0) {
+          System.out.println(fizz + buzz);
+        }
+        else if (number % 3 == 0) {
+          System.out.println(buzz);
+        }
+        else if (number % 5 == 0) {
+          System.out.println(fizz);
+        } else {
+          System.out.println(number);
+        }
+        numberOfInput--;
+      }
     }
-    else if (number % 3 == 0) {
-      System.out.println(buzz);
-    }
-    else if (number % 5 == 0) {
-      System.out.println(fizz);
-    } else {
-      System.out.println(number);
-    }
-    
+    System.out.println("Thank you for playing!");
   }
 }
