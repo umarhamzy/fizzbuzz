0a7c2d1b61ce0866d1e3e0df57e6b6a012eb4c87
umarhamzy <138566943+umarhamzy@users.noreply.github.com>
2023-08-31 10:52:55 +0530

    Add Fizz buzz program

--- src/com/codewithmosh/Main.java
+++ src/com/codewithmosh/Main.java
@@ -1,34 +1,28 @@
 package com.codewithmosh;
 
-import java.text.NumberFormat;
 import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
     
-    final byte  PERCENT = 100;
-    final byte MONTHS_IN_A_YEAR = 12;
+    String fizz = "Fizz";
+    String buzz = "Buzz";
     
-    System.out.print("Principal: ");
-    int principal = scanner.nextInt();
+    System.out.print("Number: ");
+    int number = scanner.nextInt();
     
-    System.out.print("Annual Interest Rate: ");
-    float annualInterest = scanner.nextFloat();
-    
-    System.out.print("Period (Years): ");
-    byte years = scanner.nextByte();
-    int numberOfPayments = years * MONTHS_IN_A_YEAR;
-    
-    float monthlyInterest = annualInterest/PERCENT/MONTHS_IN_A_YEAR;
-    
-    double mortgage = principal
-        * (monthlyInterest * (Math.pow((1 + monthlyInterest), numberOfPayments)))
-        / Math.pow((1 + monthlyInterest), numberOfPayments) - 1;
-    String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
-    
-    System.out.printf("Mortgage: %s", mortgageFormatted);
-    
+    if (number % 5 == 0 && number % 3 == 0) {
+      System.out.println(fizz + buzz);
+    }
+    else if (number % 3 == 0) {
+      System.out.println(buzz);
+    }
+    else if (number % 5 == 0) {
+      System.out.println(fizz);
+    } else {
+      System.out.println(number);
+    }
     
   }
 }
